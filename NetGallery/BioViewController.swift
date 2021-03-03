e4fd8d533f1021dd4c8b79e9641c2f0591c07e21
Vasiliy Samarin <aiven92@gmial.com>
2021-03-03 20:36:12 +0300

    Initial commit

---
+++ NetGallery/BioViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NetGallery
+//
+//  Created by Vasiliy Samarin on 03.03.2021.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
