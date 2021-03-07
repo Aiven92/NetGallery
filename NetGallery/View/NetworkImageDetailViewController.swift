459978e70fa8413caed6bd3b6b21b053472ca605
Vasiliy Samarin <aiven92@gmial.com>
2021-03-07 14:27:31 +0300

    Add save image button

--- NetGallery/View/NetworkImageDetailViewController.swift
+++ NetGallery/View/NetworkImageDetailViewController.swift
@@ -53,7 +53,25 @@ class NetworkImageDetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc func saveToGalleryError(_ image: UIImage, didFinishWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // Notify user about error vith aler message
+            let alert = UIAlertController(title: "Error", message: "Can't save image to gallery\n\(error)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    @objc func action(sender: UIBarButtonItem) {
+        guard let image = self.image.image else { return }
+        // For use: set "Privacy - Photo Library Additions Usage Description" in Info.plist
+        UIImageWriteToSavedPhotosAlbum(image , self, #selector(saveToGalleryError), nil)
+    }
+    
     override func viewDidLoad() {
+        // Set NavBar right button and attach func
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(self.action(sender:)))
+        
         self.addSubviews ()
         self.setupConstraints()
     }
